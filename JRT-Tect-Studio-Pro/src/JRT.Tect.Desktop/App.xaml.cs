8cd738a726ad823236f016d9042aa4ea77a42106
Ferdy valentino surya syahputra <ferdyvalentino@MacBook-Air-Ferdy.local>
2026-08-11 00:14:48 +0700

    Configure full native C# WPF & WinForms application build scripts

---
+++ JRT-Tect-Studio-Pro/src/JRT.Tect.Desktop/App.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows;
+
+namespace JRT.Tect.Desktop
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ JRT-Tect-Studio-Pro/src/JRT.Tect.Desktop/Views/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace JRT.Tect.Desktop.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
